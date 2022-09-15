f0c6b8a14689fb116d68b3f6aba5f27fdf69dd0c
mccamara <camaramamady9160@gmail.com>
2022-09-15 15:10:56 +0000

    Faire tirage

--- src/app/tirage/tirage.component.ts
+++ src/app/tirage/tirage.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Accueil } from '../services/acueil.service';
+import { HttpClient } from '@angular/common/http';
 
+export interface TirageModel{
+  idtirage: number,
+  libelletirage: string
+}
 @Component({
   selector: 'app-tirage',
   templateUrl: './tirage.component.html',
   styleUrls: ['./tirage.component.css']
 })
 export class TirageComponent implements OnInit {
-
-  constructor() { }
+  libelleliste:String='';
+  nombreatire!:number;
+  formulaireTirage! : FormGroup;
+  
+  tirageOjet: TirageModel = {
+    idtirage: 0,
+  libelletirage: ''
+  }
+nomtirage:any;
+  liste:any;
+  constructor(private service : Accueil, private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.service.getListe().subscribe(data=>{
+      console.log(data)
+      this.liste=data;
+    })
+  }
+  public tirer() {
+    const url='http://localhost:8080/tirage/createTirage';
+    this.tirageOjet.libelletirage = this.nomtirage
+    
+    this.http.post(`${url}/${this.libelleliste}/${this.nombreatire}`,this.tirageOjet).subscribe({
+      next: data=> {
+        alert('Tirage effectué avec succès!');
+        this.libelleliste='';
+      },
+      error: (e) => {
+        alert('Problème survenu lors du tirage !')
+      }
+    })
+
+
   }
 
 }
